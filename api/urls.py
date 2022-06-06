48e5b87925b2a1e873e45a2013b61406a1634778
vesellluxa <62771951+vesellluxa@users.noreply.github.com>
2022-06-06 13:56:16 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,9 @@ from rest_framework import routers
 from api.views import ArticleView
 
 
-router = routers.DefaultRouter()
+router_v1 = routers.DefaultRouter()
+router_v1.register('articles', ArticleView, basename='articles')
 
-router.register('articles', ArticleView, basename='articles')
-
-urlpatterns = []
-
-urlpatterns += router.urls
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
