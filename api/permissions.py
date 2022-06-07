9ec02d932b416b86f9c3d45e656c80134a33efbd
vesellluxa <62771951+vesellluxa@users.noreply.github.com>
2022-06-07 12:46:03 +0300

    Update permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -9,8 +9,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
                     and request.user.role == ArticleUser.AUTHOR)
-                or request.user.is_authenticated
-                and request.user.is_superuser)
+                or (request.user.is_authenticated
+                and request.user.is_superuser))
 
     def has_object_permission(self, request, view, obj):
         return ((request.method in permissions.SAFE_METHODS)
